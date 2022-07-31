03a920c81d1f2b0f472e4ef85ce9b2c5accbf995
tanvir477 <tanvirhassan477@gmail.com>
2022-08-01 02:08:11 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var app = Vue.createApp({
+    data(){
+        return{
+            msg: "welcome to vue",
+            tutotialInfo:{
+                name: "vue js 3 bangla tutorial",
+                githubLink : "github@gmail.com"
+                
+            }
+        };
+    }
+})
+
+app.mount('#app');
