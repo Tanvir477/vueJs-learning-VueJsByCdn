9c46bdfb592881fea12d09c3509456d9317af72c
tanvir477 <tanvirhassan477@gmail.com>
2022-08-01 22:50:10 +0600

    event

--- app.js
+++ app.js
@@ -4,11 +4,26 @@ var app = Vue.createApp({
             msg: "welcome to vue",
             tutotialInfo:{
                 name: "vue js 3 bangla tutorial",
-                githubLink : "github@gmail.com"
-                
-            }
+                githubLink : "github@gmail.com",
+            },
+            htmlCode: '<a href="https://github.com/Tanvir477/vueJs-learning-VueJsByCdn">HTMLGithub</a>',
+
+            count : 0,
         };
-    }
+    },
+
+    methods: {
+      getCurrentTime(){
+        let ct = new Date()
+        return new Date()
+      },
+      increase(){
+        this.count++;
+      },
+      decrease(){
+        this.count--;
+      },
+    },
 })
 
 app.mount('#app');
